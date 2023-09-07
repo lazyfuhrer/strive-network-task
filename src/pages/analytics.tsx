f7d4dfcc4ffc6d68ff3a79359e3fc49f2e3506ce
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-09-08 05:24:12 +0530

    feat: created 2 pages

---
+++ src/pages/analytics.tsx
@@ -0,0 +1,12 @@
+import { Center, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Analytics() {
+  return (
+    <>
+        <Center p={20}>
+            <Heading>Not part of the assignment</Heading>
+        </Center>
+    </>
+  )
+};
---
+++ src/pages/logout.tsx
@@ -0,0 +1,12 @@
+import { Center, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Logout() {
+  return (
+    <>
+        <Center p={20}>
+            <Heading>Not part of the assignment</Heading>
+        </Center>
+    </>
+  )
+};
