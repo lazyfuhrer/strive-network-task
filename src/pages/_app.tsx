7dcd08322d5f896d608d6cade9e1be4ab1e8663a
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-09-14 00:52:06 +0530

    feat: added walletconnect

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,56 @@ import Layout from '@/components/Layout';
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  darkTheme,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { polygonMumbai, polygon } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
 const quicksand = Quicksand({ subsets: ['latin'] })
 
+const { chains, publicClient } = configureChains(
+  [polygonMumbai],
+  [
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID! }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Strive Network App',
+  projectId: '62f4b0df50c4e1733ead674371cabf77',
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <main className={quicksand.className}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </main>
-    </ChakraProvider>
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} theme={darkTheme({
+        accentColor: 'rgba(0, 120, 126, 1)',
+        fontStack: 'system',
+        overlayBlur: 'small',
+      })}
+      >
+        <ChakraProvider>
+          <main className={quicksand.className}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </main>
+        </ChakraProvider>
+      </RainbowKitProvider>  
+    </WagmiConfig>  
   );
 };
